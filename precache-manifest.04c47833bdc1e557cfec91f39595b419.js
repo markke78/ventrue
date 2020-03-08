02886db4722b83b305a1b36a76d510caafd9225d
markke78 <56062146+markke78@users.noreply.github.com>
2020-03-08 13:27:58 -0600

    Updates

---
+++ precache-manifest.04c47833bdc1e557cfec91f39595b419.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921835c3c34fc4031222d83a805811ac",
+    "url": "/venture/index.html"
+  },
+  {
+    "revision": "40abc7f27d0b9249f7de",
+    "url": "/venture/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1ed7643ca3353e748ef3",
+    "url": "/venture/static/css/main.bab82aee.chunk.css"
+  },
+  {
+    "revision": "40abc7f27d0b9249f7de",
+    "url": "/venture/static/js/2.3e4ce632.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/venture/static/js/2.3e4ce632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed7643ca3353e748ef3",
+    "url": "/venture/static/js/main.d5058b0e.chunk.js"
+  },
+  {
+    "revision": "46d1fd05896efc027f4d",
+    "url": "/venture/static/js/runtime-main.a5e2347a.js"
+  },
+  {
+    "revision": "8131030d594da576c9a8d1ae92e6d7b1",
+    "url": "/venture/static/media/about.8131030d.jfif"
+  },
+  {
+    "revision": "d77715c3679cb995f9a63e5c7dca0328",
+    "url": "/venture/static/media/book.d77715c3.png"
+  },
+  {
+    "revision": "8d789dfeeac72415279c37be275d2f3f",
+    "url": "/venture/static/media/mountains2.8d789dfe.jpg"
+  }
+]);
